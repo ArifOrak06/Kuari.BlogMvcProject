6a5441b7f01c91303c93027afbdee4441c15768f
Arif ORAK <ariforak91@gmail.com>
2023-03-11 21:47:03 +0300

    CategorySeedDataAdded Migration Snapshot Added.

--- Kuari.Blog.Repository/Migrations/20230311184609_CategorySeedDataAdded.Designer.cs
+++ Kuari.Blog.Repository/Migrations/20230311184609_CategorySeedDataAdded.Designer.cs
@@ -4,6 +4,7 @@ using Kuari.Blog.Repository.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kuari.Blog.Repository.Migrations
 {
     [DbContext(typeof(BlogDbContext))]
-    partial class BlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230311184609_CategorySeedDataAdded")]
+    partial class CategorySeedDataAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Kuari.Blog.Repository/Migrations/20230311184609_CategorySeedDataAdded.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kuari.Blog.Repository.Migrations
+{
+    public partial class CategorySeedDataAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name", "Status" },
+                values: new object[,]
+                {
+                    { 1, "Yazılım Dünyasinda Kariyer Fırsatları", "Yazılım", true },
+                    { 2, "Sinema ve Tiyatro Dünyasındaki Son Gelişmeler", "Sinema", true },
+                    { 3, "Sağlıklı Hayat", "Sağlık", true },
+                    { 4, "Ulusal ve Uluslararası Spor Müsabakaları", "Spor", true },
+                    { 5, "Ulusal Siber Güvenliğin Önemi", "Siber Güvenlik", true }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
