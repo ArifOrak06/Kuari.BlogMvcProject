5b6fed3719ddd2cb60581f48e597c6eabe6b19fa
Arif ORAK <ariforak91@gmail.com>
2023-03-11 18:46:31 +0300

    AutoMapper Refactory

--- Kuari.Blog.Service/Mappings/AutoMapper/DtoMapper.cs
+++ Kuari.Blog.Service/Mappings/AutoMapper/DtoMapper.cs
@@ -1,13 +1,11 @@
 ﻿using AutoMapper;
+using Kuari.Blog.Core.DTOs.Concrete.AboutDTOs;
 using Kuari.Blog.Core.DTOs.Concrete.ArticleDTOs;
+using Kuari.Blog.Core.DTOs.Concrete.AuthorDTOs;
 using Kuari.Blog.Core.DTOs.Concrete.CategoryDTOs;
 using Kuari.Blog.Core.DTOs.Concrete.CommentDTOs;
+using Kuari.Blog.Core.DTOs.Concrete.ContactDTOs;
 using Kuari.Blog.Core.Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kuari.Blog.Service.Mappings.AutoMapper
 {
@@ -17,10 +15,22 @@ namespace Kuari.Blog.Service.Mappings.AutoMapper
         {
             CreateMap<ArticleDto, Article>().ReverseMap();
             CreateMap<ArticleUpdateDto, Article>().ReverseMap();
+
             CreateMap<CategoryDto, Category>().ReverseMap();
             CreateMap<CategoryUpdateDto, Category>().ReverseMap();
+
             CreateMap<CommentDto, Comment>().ReverseMap();
             CreateMap<CommentUpdateDto, Comment>().ReverseMap();
+
+            CreateMap<AuthorDto, Author>().ReverseMap();
+            CreateMap<AuthorUpdateDto, Author>().ReverseMap();
+
+            CreateMap<ContactDto, Contact>().ReverseMap();
+            CreateMap<ContactUpdateDto, Contact>().ReverseMap();
+
+            CreateMap<AboutDto, About>().ReverseMap();
+            CreateMap<AboutUpdateDto, About>().ReverseMap();
+
         }
     }
 }
